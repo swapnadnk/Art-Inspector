28403c9e4740c1745a198bd2650de9ea2c232fdf
Swapna Dirisala <sdirisala@apple.com>
2025-07-28 10:43:34 -0700

    Initial Commit

---
+++ Art-Inspector/Art_InspectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_InspectorApp.swift
+//  Art-Inspector
+//
+//  Created by Swapna Dirisala on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Art_InspectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
